a5cef11414fc86f7fac8471321337312a75714a6
Choibw <den01@naver.com>
2025-07-23 17:28:36 +0900

    Initial commit: Unreal project setup

---
+++ Source/WashingMan/WashingMan.Build.cs
@@ -0,0 +1,43 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class WashingMan : ModuleRules
+{
+	public WashingMan(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] {
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"EnhancedInput",
+			"AIModule",
+			"StateTreeModule",
+			"GameplayStateTreeModule",
+			"UMG"
+		});
+
+		PrivateDependencyModuleNames.AddRange(new string[] { });
+
+		PublicIncludePaths.AddRange(new string[] {
+			"WashingMan",
+			"WashingMan/Variant_Platforming",
+			"WashingMan/Variant_Combat",
+			"WashingMan/Variant_Combat/AI",
+			"WashingMan/Variant_SideScrolling",
+			"WashingMan/Variant_SideScrolling/Gameplay",
+			"WashingMan/Variant_SideScrolling/AI"
+		});
+
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
---
+++ Source/WashingMan/WashingManGameMode.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "WashingManGameMode.h"
+
+AWashingManGameMode::AWashingManGameMode()
+{
+	// stub
+}
