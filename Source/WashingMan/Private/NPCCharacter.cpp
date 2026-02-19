13d60cedcf19202a2fff1425f16f4980f16a698e
Choibw <den01@naver.com>
2026-02-19 22:26:16 +0900

    npc가 이동할 때 이동 방향을 바라보게 변경

--- Source/WashingMan/Private/NPCCharacter.cpp
+++ Source/WashingMan/Private/NPCCharacter.cpp
@@ -2,12 +2,31 @@
 
 
 #include "NPCCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
 // Sets default values
 ANPCCharacter::ANPCCharacter()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
+
+	// 컨트롤러 회전 끄기
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationRoll = false;
+
+	// 이동 방향으로 캐릭터가 회전하게
+	UCharacterMovementComponent* MoveComp = GetCharacterMovement();
+	if (MoveComp)
+	{
+		MoveComp->bOrientRotationToMovement = true;
+
+		// 회전 속도(튜닝 포인트) - 값 클수록 더 빨리 고개 돌림
+		MoveComp->RotationRate = FRotator(0.f, 540.f, 0.f);
+
+		// (선택) 이동 가속/감속 느낌 튜닝도 여기서 가능
+		// MoveComp->MaxWalkSpeed = 200.f;
+	}
 
 }
 
